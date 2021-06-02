0223c4c6969837ec453a64ec8c2eca42d5fc7d91
Dinu Ionica <dinuionica_10@yahoo.com>
2021-06-02 20:39:47 +0300

    Concat and Split Implementations

--- rope.c
+++ rope.c
@@ -1,3 +1,6 @@
+/* 315CA Dinu Ion Irinel 
+ * 315CA Serban Bianca-Sanziana
+ */
 #include "./rope.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -7,10 +10,11 @@
 
 #define EMPTY ""
 
-RopeNode *makeRopeNode(const char *str)
-{
+char *strdup(const char *s);
 
-    /* crearea nodului */
+/* functie pentru crearea unui nou nod */
+RopeNode *makeRopeNode(const char *str) {
+    /* alocarea memoriei pentru nod */
     RopeNode *node = malloc(sizeof(RopeNode));
     DIE(node == NULL, "Allocation error rope_node\n");
 
@@ -20,16 +24,16 @@ RopeNode *makeRopeNode(const char *str)
 
     /* actualizare valoare string */
     node->str = str;
-    /* actualizare inaltime */
-    node->weight = 0;
+
+    /* actualizare greutate nod cu lungimea string-ului */
+    node->weight = strlen(str);
 
     return node;
 }
 
-RopeTree *makeRopeTree(RopeNode *root)
-{
-
-    /* creare arbore */
+/* functie pentru crearea unui nou rope */
+RopeTree *makeRopeTree(RopeNode *root) {
+    /* alocarea memoriei pentru rope */
     RopeTree *rope_tree = malloc(sizeof(RopeTree));
     DIE(rope_tree == NULL, "Allocation error rope_tree\n");
 
@@ -39,13 +43,13 @@ RopeTree *makeRopeTree(RopeNode *root)
     return rope_tree;
 }
 
-void printRopeNode(RopeNode *rn)
-{
-    if (!rn)
+/* functii pentru afisarea string-ului unui nod */
+void printRopeNode(RopeNode *rn) {
+    if (!rn) {
         return;
+    }
 
-    if (!(rn->left) && !(rn->right))
-    {
+    if (!(rn->left) && !(rn->right)) {
         printf("%s", rn->str);
         return;
     }
@@ -54,84 +58,146 @@ void printRopeNode(RopeNode *rn)
     printRopeNode(rn->right);
 }
 
-void printRopeTree(RopeTree *rt)
-{
-    if (rt && rt->root)
-    {
+void printRopeTree(RopeTree *rt) {
+    if (rt && rt->root) {
         printRopeNode(rt->root);
         printf("%s", "\n");
     }
 }
 
-void debugRopeNode(RopeNode *rn, int indent)
-{
-    if (!rn)
+/* functie pentru afisarea informatiilor unui nod */
+void debugRopeNode(RopeNode *rn, int indent) {
+    if (!rn) {
         return;
-
-    for (int i = 0; i < indent; ++i)
+    }
+    for (int i = 0; i < indent; ++i) {
         printf("%s", " ");
+    }
 
-    if (!strcmp(rn->str, EMPTY))
+    if (!strcmp(rn->str, EMPTY)) {
         printf("# %d\n", rn->weight);
-    else
+    } else {
         printf("%s %d\n", rn->str, rn->weight);
+    }
 
     debugRopeNode(rn->left, indent + 2);
     debugRopeNode(rn->right, indent + 2);
 }
 
-int getTotalWeight(RopeNode *rt)
-{
-    if (!rt)
-        return 0;
-
-    return rt->weight + getTotalWeight(rt->right);
+/* functie pentru crearea unei copii a unui rope */
+RopeNode *copy_rope(RopeNode *rn) {
+    if (!rn) {
+        return NULL;
+    }
+    RopeNode *new_rn = makeRopeNode(strdup(rn->str));
+    new_rn->left = copy_rope(rn->left);
+    new_rn->right = copy_rope(rn->right);
+    new_rn->weight = strlen(new_rn->str) + total_weight(new_rn->left);
+    return new_rn;
 }
 
-RopeTree *concat(RopeTree *rt1, RopeTree *rt2)
-{
-    // TODO 1. Concat - 10p
+/* functie pentru calcularea greutatii unui nod */
+int total_weight(RopeNode *rt) {
+    if (!rt) {
+        return 0;
+    }
+    return rt->weight + total_weight(rt->right);
+}
 
-    /* crearea noului rope */
-    RopeTree *rt3 = malloc(sizeof(RopeTree));
-    rt3->root = malloc(sizeof(RopeNode));
+/* functie pentru concatenarea a doua rope-uri pe baza a doua ropuri intiale */
+RopeTree *concat(RopeTree *rt1, RopeTree *rt2) {
+    /* TODO(Dinu Ion Irinel) */
+    /* crearea unei noi radacini si a unui nou rope */
+    RopeNode *rt3_root = makeRopeNode((char *)strdup(EMPTY));
+    RopeTree *rt3 = makeRopeTree(rt3_root);
 
-    /* actualizarea pointerilor left si right */
+    /* actualizarea pointerilor copii left si right */
     (rt3->root)->left = rt1->root;
     (rt3->root)->right = rt2->root;
 
-    /* actualizare greutate */
-    (rt3->root)->weight = getTotalWeight(rt1->root->left);
-
-    (rt3->root)->str = NULL;
+    /* actualizarea noii greutati */
+    (rt3->root)->weight = total_weight(rt3->root->left);
 
     return rt3;
 }
 
-char indexRope(RopeTree *rt, int idx)
-{
-    // TODO 2. Index - 10p
-    
+/* functie pentru concatenarea a doua rope-uri pe baza a doua noduri intiale */
+RopeNode *concat_nodes(RopeNode *rt1, RopeNode *rt2) {
+    /* TODO(Dinu Ion Irinel) */
+
+    /* crearea unui nou nod */
+    RopeNode *rt3 = makeRopeNode((char *)strdup(EMPTY));
+
+    /* actualizarea pointerilor left si right */
+    rt3->left = rt1;
+    rt3->right = rt2;
+
+    /* actualizarea noii greutati */
+    rt3->weight = total_weight(rt3->left);
+
+    return rt3;
 }
 
-char *search(RopeTree *rt, int start, int end)
-{
-    // TODO 3. Search - 20p
+
+/* functii folosite pentru a imparti un rop in doua rope-uri separate */
+SplitPair split(RopeTree *rt, int idx) {
+    /* TODO(Dinu Ion Irinel) */
+    /* crearea unui nod care pointeaza la o copie a rope-ului
+     * initial care in final va reprezenta rope-ul din partea stanga
+     */
+    RopeNode *left_rn = copy_rope(rt->root);
+    /* crearea nodului care pointeaza la rope-ul drept dorit */
+    RopeNode *right_rn = makeRopeNode((char *)strdup(EMPTY));
+
+    __split(&left_rn, &right_rn, idx);
+
+    if (left_rn == NULL) {
+        left_rn = makeRopeNode(strdup(EMPTY));
+    }
+
+    /* returnarea celor doua noduri obtinute dupa impartire */
+    SplitPair pair;
+    pair.left = left_rn;
+    pair.right = right_rn;
+    return pair;
 }
 
-SplitPair split(RopeTree *rt, int idx)
-{
-    // TODO 4. Split - 20p
+void __split(RopeNode **left_rn, RopeNode **right_rn, int idx) {
+    /* concatenarea celor doua noduri daca index-ul are o valoare negativa */
+    if (idx <= 0) {
+        *right_rn = concat_nodes(*right_rn, *left_rn);
+        *left_rn = NULL;
+        return;
+    }
+    if (idx < (int) strlen((*left_rn)->str)) {
+        /* copiere string-ului stang intr-un string temporar */
+        char *left_string = malloc((idx + 1) * sizeof(char));
+        DIE(left_string == NULL, "Allocation error left_string\n");
+        strncpy(left_string, (*left_rn)->str, idx);
+        /* adaugare terminator de sir */
+        left_string[idx] = '\0';
+        /* crearea celor doua noduri temporarele  */
+        RopeNode *aux_left_node = makeRopeNode(strdup(left_string));
+        RopeNode *aux_right_node = makeRopeNode(strdup((*left_rn)->str + idx));
+
+        /* eliberarea memoriei string-urilor si a nodului stang */
+        free(left_string);
+        free((void *)(*left_rn)->str);
+        free((*left_rn));
+        /* concatenarea celor doua noduri auxiliare */
+        (*left_rn) = concat_nodes(aux_left_node, aux_right_node);
+    }
+
+    /* apelare functie pentru fiul stang */
+    if ((*left_rn)->left != NULL && (*left_rn)->weight > idx) {
+        __split(&(*left_rn)->left, right_rn, idx);
+    }
+    /* apelare functie pentru fiul drept */
+    if ((*left_rn)->right != NULL) {
+        __split(&(*left_rn)->right, right_rn, idx - (*left_rn)->weight);
+    }
+
+    /* actualizarea noii greutati */
+    (*left_rn)->weight = total_weight((*left_rn)->left);
 }
 
-RopeTree *insert(RopeTree *rt, int idx, const char *str)
-{
-    // TODO 5. Insert - 5p
-}
-
-RopeTree *delete (RopeTree *rt, int start, int len)
-{
-    // TODO 6. Delete - 5p
-}
-
-// FINAL 10p -> complex test involving all operations
